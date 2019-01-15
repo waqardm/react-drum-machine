3593211f2941ce92bbcb5897b25a26435bcfb0d6
Waqar Mohammad <waqar@waqar.co>
2019-01-15 12:10:51 +0000

    Imported bulma

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bulma-components/full';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p className="title is-1">
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
-      </div>
-    );
+      </div>;
   }
 }
 
